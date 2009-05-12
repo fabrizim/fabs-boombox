4112b49f4f08b24f4ecf741adc4ad7c312433caf
mark.fabrizio <mark.fabrizio@12581942-39b2-11de-b310-9106b1f7c239>
2009-05-12 20:56:53 +0000

    Added an index page to the examples. Updated the examples with proper titles / headings.

--- examples/frames.php
+++ examples/frames.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-gb" lang="en-gb" >
     <head>
-        <title>Frames test</title>
+        <title>Fab's Boombox - Frames Example</title>
     </head>
     <frameset rows="*,0" frameborder="0" framespacing="0">
         <frame name="content" src="content-frame.php" frameborder="0" marginheight="0" marginwidth="0" />
